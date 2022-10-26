7e025cc4b108a31f81e97ac17cfb809111c78716
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-26 14:21:35 -0700

    Finished part 1

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -11,17 +11,39 @@ class Queue {
     enqueue(val) {
         // Add node to end of queue (linked list)
 
-        // Your code here
+        let newNode = new SinglyLinkedNode(val);
 
+        if (!this.head && !this.tail) {
+          this.head = newNode;
+          this.tail = newNode;
+        } else if (this.length === 1) {
+          this.tail = newNode;
+          this.head.next = this.tail;
+        } else {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        }
+        this.length++;
+        return this.length;
         // Write your hypothesis on the time complexity of this method here
+        // O(1), no iteration, just constant operations
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
 
-        // Your code here
-
+        if (!this.head && !this.tail) return null;
+        let removed = this.head.value;
+        if (this.length === 1) {
+          this.head = null;
+          this.tail = null;
+        } else {
+          this.head = this.head.next;
+        }
+        this.length--;
+        return removed;
         // Write your hypothesis on the time complexity of this method here
+        // O(1), constant operations, no iteration
     }
 
 }
